a82bf4aa78cac4b1c6eccd47f9164de5ea12af72
Nicolas Mailloux <nicolecrivain@gmail.com>
2021-04-02 22:12:02 -0400

    Fixed early scaling issue

--- main.cpp
+++ main.cpp
@@ -8,8 +8,8 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     diagsWindow w;
-    w.show();
     const QScreen* screen = qApp->primaryScreen();
     w.setGeometry(QRect(QPoint(0,0), screen->geometry ().size()));
+    w.show();
     return a.exec();
 }
